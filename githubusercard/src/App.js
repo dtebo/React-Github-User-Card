c7bbdbab8460f5958c9c3d36ef1d2039bd70f1ca
Darren Tebo <darrentebo83@outlook.com>
2020-07-09 21:21:33 -0400

    displayed header and updated global default font size

--- githubusercard/src/App.js
+++ githubusercard/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/header/Header';
+
+let baseUrl = 'https://api.github.com/users/';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      userdata: ''
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`${baseUrl}dtebo`)
+      .then(resp => {
+        console.log('axios success: ', resp);
+      })
+      .catch(err => {
+        console.log('axios error: ', err);
+      });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
